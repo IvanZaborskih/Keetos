c2de0f2449bfce84a9da62ade2e4a252c165eaf2
IvanZaborskih <ivan.31.03@mail.ru>
2021-09-11 16:03:35 +0300

    add new parallax on About

--- js/app.js
+++ js/app.js
@@ -105,19 +105,43 @@ VANTA.FOG({
     speed: 5.00,
     zoom: 0.30
 });
-// VANTA.FOG({
-//     el: "#first--screen",
-//     mouseControls: true,
-//     touchControls: true,
-//     gyroControls: false,
-//     minHeight: 200.00,
-//     minWidth: 200.00,
-//     highlightColor: 0x77ff,
-//     midtoneColor: 0x83ff,
-//     lowlightColor: 0x75ff,
-//     baseColor: 0x0,
-//     blurFactor: 0.90,
-//     speed: 5.00,
-//     zoom: 0.30
-// });
+
+
+// Parallax
+$('.img-parallax').each(function () {
+    var img = $(this);
+    var imgParent = $(this).parent();
+    function parallaxImg() {
+        var speed = img.data('speed');
+        var imgY = imgParent.offset().top;
+        var winY = $(this).scrollTop();
+        var winH = $(this).height();
+        var parentH = imgParent.innerHeight();
+
+
+        // The next pixel to show on screen
+        var winBottom = winY + winH;
+
+        // If block is shown on screen
+        if (winBottom > imgY && winY < imgY + parentH) {
+            // Number of pixels shown after block appear
+            var imgBottom = ((winBottom - imgY) * speed);
+            // Max number of pixels until block disappear
+            var imgTop = winH + parentH;
+            // Porcentage between start showing until disappearing
+            var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+        }
+        img.css({
+            top: imgPercent + '%',
+            transform: 'translate(-50%, -' + imgPercent + '%)'
+        });
+    }
+    $(document).on({
+        scroll: function () {
+            parallaxImg();
+        }, ready: function () {
+            parallaxImg();
+        }
+    });
+});
 
